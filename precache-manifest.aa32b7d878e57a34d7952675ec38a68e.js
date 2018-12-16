7c633d0f2bcb9af5ad2f11ae5d9b1dd2957d4d67
Yulia <homka15@i.ua>
2018-12-16 16:16:27 +0200

    Updates

---
+++ precache-manifest.aa32b7d878e57a34d7952675ec38a68e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "7699c519fb00d37c6cb9",
+    "url": "/react-app/static/js/main.7699c519.chunk.js"
+  },
+  {
+    "revision": "70b4cd72fa66dca2c008",
+    "url": "/react-app/static/js/1.70b4cd72.chunk.js"
+  },
+  {
+    "revision": "7699c519fb00d37c6cb9",
+    "url": "/react-app/static/css/main.bb6dedca.chunk.css"
+  },
+  {
+    "revision": "ace8e0713187ffc145d5d7101afd482a",
+    "url": "/react-app/index.html"
+  }
+];
